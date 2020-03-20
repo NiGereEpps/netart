ece39f518a6ffe7f192bc124806d687089353c09
NiGereEpps <nigereepps@uchicago.edu>
2020-03-20 18:32:18 -0500

    Final Product
    Added gallery page and countdown for sc

--- js/vrfunctions.js
+++ js/vrfunctions.js
@@ -1,23 +1,24 @@
-/*
-var page=3;
-
-function nextPage () {
-    window.location.href = 'webvr' + page + '.html';
-    page++;
-}
-*/
-
-function page3 () {
-    window.location.href = 'webvr3.html'
-}
-
-function page4 () {
-    window.location.href = 'webvr4.html'
-}
-
 function finish(){
-    window.location.href = "statement.html"
+    window.location.href = "gallery.html"
 }
+
+function restart(){
+    window.location.href = "../index.html"
+}
+
+function openPage(page){
+    if (sessionStorage.viewmode == 'true') { //if statement not needed
+        window.open('webvr' + page + '.html', '_blank');   
+    }
+}
+
+function changePage(page){
+    if (sessionStorage.viewmode != 'true') { // == 'false'
+        setTimeout(function(){window.location.href = 'webvr' + page + '.html'}
+            , 30*1000);
+    }
+}
+   
 
 //onclick to switch page and start timer
 AFRAME.registerComponent('start-scenes', {
@@ -31,3 +32,23 @@ AFRAME.registerComponent('start-scenes', {
         });
     }
   });
+
+AFRAME.registerComponent('countdown', {
+    init: function () {
+
+        var el = this.el;
+        var seconds = 30;
+
+        var countdown = setInterval(function() {
+            el.setAttribute("value", seconds);
+            seconds--;
+
+            if(seconds == 0){
+                clearInterval(countdown);
+            }
+        }, 1000);       
+         
+
+    }
+       
+});
